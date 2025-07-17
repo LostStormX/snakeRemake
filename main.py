d9ca6f809382ab22dc7d4265f4260d39d59d9deb
LostStormX <boylestonh@icloud.com>
2025-07-17 17:41:46 -0400

    FIXED continious movement

--- main.py
+++ main.py
@@ -10,71 +10,63 @@ pygame.display.set_caption("Snake Remake - V 1.0.0")
 pygame.display.set_icon(pygame.image.load("Logo.png"))
 
 clock = pygame.time.Clock()
-running = True
+run = True
 deltaTime = 0
-speed = 0.1
+speed = 5
 score = 0
+length = 1
+
+direction = pygame.Vector2(0,0)
 
 player_pos = pygame.Vector2(screen.get_width() / 2 , screen.get_height() / 2)
 
-while running:
+while run:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            run = False
+        if event.type == pygame.KEYDOWN:
+            print(pygame.key.name(event.key))
+            if event.key == pygame.K_UP:
+                print(pygame.key.name(event.key))
 
     screen.fill("#009b34")
 
     # Text
     font = pygame.font.SysFont("Arial", 30, True,False)
-    textSurface = font.render("Score:", False, ("black"))
-
+    textSurface = font.render("Score: " + str(score), False, "black")
 
     # Draw the player obj
     player = pygame.Rect(player_pos.x, player_pos.y,30,30)
     player.centerx = player_pos.x
     player.centery = player_pos.y
 
-    # Draw the boundaries
-    boundary = pygame.Rect(0,0,700,500)\
-
-    # Collission
-    #collide = boundary.colliderect(player)
-    #if collide:
-    #    score += 1
-    #    print(score)
-
-
-    keys = pygame.key.get_pressed()
-
+    boundary = pygame.Rect(0,0,700,500)
 
 
     # Implementing Arrow Keys
-    if keys[pygame.K_UP]:
-        while True:
-            player_pos.y -= speed * deltaTime
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_UP] and direction.y == 0:
+        direction = pygame.Vector2(0, -1)
+    if keys[pygame.K_DOWN] and direction.y == 0:
+        direction = pygame.Vector2(0, 1)
+    if keys[pygame.K_LEFT] and direction.x == 0:
+        direction = pygame.Vector2(-1, 0)
+    if keys[pygame.K_RIGHT] and direction.x == 0:
+        direction = pygame.Vector2(1, 0)
 
 
-    if keys[pygame.K_DOWN]:
-        player_pos.y += speed * deltaTime
-
-
-    if keys[pygame.K_LEFT]:
-        player_pos.x -= speed * deltaTime
-
-
-    if keys[pygame.K_RIGHT]:
-        player_pos.x += speed * deltaTime
-
+    player_pos += direction * speed
 
     # Render Shapes
     pygame.draw.rect(screen, "#006c24", player)
     pygame.draw.rect(screen, "#006c24", boundary, 35)
 
     # Render text
-    screen.blit(textSurface,(280, 0))
-
+    screen.blit(textSurface, (280, 0))
 
     pygame.display.flip()
     deltaTime = clock.tick(30)
-
 pygame.quit()
+quit()
